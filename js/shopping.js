d56dcfc5f1179e2db614f001845a18432a838c3a
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-08-20 20:14:21 +0600

    overall done

--- js/shopping.js
+++ js/shopping.js
@@ -11,28 +11,62 @@ function Change(src, descValue, priceValue) {
   priceText.innerText = priceValue;
 }
 
+// show function
+function showDeliveryCost(issRegular) {
+  // regular cost
+  const regularDeliveryCost = parseInt(document.getElementById('regular').innerText);
+
+  // express cost
+  const expressDeliveryCost = parseInt(document.getElementById('express').innerText);
+
+  // current cost
+  const currentCostId = document.getElementById('delivery-cost');
+  //   set cost
+  if (issRegular == true) {
+    currentCostId.innerText = regularDeliveryCost;
+  } else {
+    currentCostId.innerText = expressDeliveryCost;
+  }
+}
+
+// show total
+function showTotal() {
+  const price = parseInt(document.getElementById('price').innerText);
+  const shipping = parseInt(document.getElementById('delivery-cost').innerText);
+  const currentTotal = document.getElementById('Total-cost');
+  const total = price + shipping;
+  currentTotal.innerText = total;
+}
+
 //<-- -------------shipping event handling------------- -->
 // for regular-->
-document.getElementById('regular-button').addEventListener('click', function () {});
+document.getElementById('regular-button').addEventListener('click', function () {
+  showDeliveryCost(true);
+  showTotal();
+});
 
 // for express-->
-document.getElementById('express-button').addEventListener('click', function () {});
+document.getElementById('express-button').addEventListener('click', function () {
+  showDeliveryCost(false);
+  showTotal();
+});
 
 //<-- -------------- Photo change event handling-------------- -->
 
 // first photo-->
 document.getElementById('first').addEventListener('click', function () {
-  //    change
   Change('images/1.jpg', 'First-Image desc:', 700);
+  showTotal();
 });
 
 // second photo-->
 document.getElementById('second').addEventListener('click', function () {
-  //   change
   Change('images/2.jpg', 'Second-Image desc:', 600);
+  showTotal();
 });
+
 // third photo-->
 document.getElementById('third').addEventListener('click', function () {
-  // change
   Change('images/3.jpg', 'First Image desc:', 500);
+  showTotal();
 });
